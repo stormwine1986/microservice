25fc0045df26a736a06175db7a3733e1e9608580
stormwine1986 <47297412+stormwine1986@users.noreply.github.com>
2019-05-28 12:29:17 +0800

    1212

--- src/main/java/demo/msa/HelloController.java
+++ src/main/java/demo/msa/HelloController.java
@@ -1,14 +1,24 @@
 package demo.msa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
-    @RequestMapping(method=RequestMethod.GET, path="/hello")
-    public String index() {
+    @RequestMapping(method=RequestMethod.GET, path="/provider")
+    public String provider() {
         return "MSA Say: Hello!!";
+    }
+
+    @RequestMapping(method=RequestMethod.GET, path="/customer")
+    public String customer() {
+        return restTemplate.getForEntity("http://MICRO-SERVICE/provider", String.class).getBody();
     }
 }
